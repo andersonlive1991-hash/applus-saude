7165509e70b25108c754569a30682bde16240414
Anderson Barros <anderson@applus.saude>
2026-05-18 21:37:05 -0300

    fix: desabilitar cache de JS e HTML no servidor

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,13 @@ const io = new Server(server, { cors: { origin: '*' } });
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, '../frontend'), {
+  setHeaders: function(res, filePath) {
+    if (filePath.endsWith('.js') || filePath.endsWith('.html')) {
+      res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+    }
+  }
+}));
 
 // Rotas
 app.use('/api/familias', require('./routes/familias'));
